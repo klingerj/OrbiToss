f837f774ca1d89299259a5d8ba149b0b1a821a04
Joseph Klinger <klingerj@seas.upenn.edu>
2018-11-26 14:19:26 -0500

    created project

---
+++ OrbiToss/Source/OrbiToss/OrbiTossGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OrbiTossGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORBITOSS_API AOrbiTossGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
