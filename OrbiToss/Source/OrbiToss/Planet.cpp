0e541295df234827b05ecd8aa220d9cd172f1ff2
Jake Snipes <jsnipes@seas.upenn.edu>
2018-11-27 23:16:29 -0500

    Working two-body simulation

---
+++ OrbiToss/Source/OrbiToss/Planet.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Planet.h"
+
+
+// Sets default values
+APlanet::APlanet() : mass(rand() % 100 + 1), radius(rand() % 3001 + 4000), considerForce(true), pos(500, 0, 0), vel(0, 50, 0), acc(0, 0, 0), force(0, 0, 0)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlanet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlanet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (vel.Size() > 0) {
+		// TODO: Query all active planets in some planet vector you create in GameManager
+		// Create std::vector<APlanet> that stores all planets and loop over
+			// if (considerForce && distanceBetween != 0)
+				// totalForce += G*m1*m2 / (distanceBetween * distanceBetween)
+	}
+
+	// TEST: Have the planet orbit a nonexistent sun located at the origin
+	FVector dist = pos - FVector(0, 0, 0); // Vector from sun to planet
+	FVector dir = dist;
+	dir.Normalize(); // Unit vector pointing from sun to planet
+	force = -(G * mass * 20000) / (dist.Size() * dist.Size() * 0.01) * dir; // Apply Law of Universal Gravitation
+
+	// Euler integration
+	acc = force / mass;
+	vel = vel + acc * DeltaTime;
+	pos = pos + vel * DeltaTime;
+
+	// Update position and velocity accordingly
+	GetRootComponent()->ComponentVelocity = vel;
+	SetActorLocation(pos);
+}
+
---
+++ OrbiToss/Source/OrbiToss/Planet.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include <cmath>
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Planet.generated.h"
+
+UCLASS()
+class ORBITOSS_API APlanet : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APlanet();
+
+	// Universal Gravitational Constant, modified to fit our simulation
+	const float G = 6.67408; // SI units: 6.67408e-11 m^3/(kg * s^2); this way, all forces will be returned as F * 10^31. We will drop the coefficient
+	
+	// Unchanging aspects of planet
+	// Mass, in (10^24) kg
+	float mass;
+	// Radius, in (10^3) m
+	float radius;
+
+	// If true, consider this planet/body in the force calculations of other bodies
+	bool considerForce;
+
+	// Changing aspects of planet
+	FVector pos, vel, acc;
+	FVector force;
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
